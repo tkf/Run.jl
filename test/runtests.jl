5fc07ee2abda43d071a6ec6293d50d514962f875
Takafumi Arakaki <aka.tkf@gmail.com>
2019-07-12 21:12:09 -0700

    Fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,13 @@ using Test
 @testset "smoke test" begin
     @test Run.docs(joinpath(@__DIR__, "..", "docs")) isa Any
     @test Run.test(PackageSpec(
-        name = "UniversalIdentity",
-        url = "https://github.com/tkf/UniversalIdentity.jl",
+        name = "InitialValues",
+        url = "https://github.com/tkf/InitialValues.jl",
+        rev = "62b240fb85836b9731b0a893e446a9c90311afce",
     )) isa Any
     @test Run.test(PackageSpec(
-        name = "UniversalIdentity",
-        url = "https://github.com/tkf/UniversalIdentity.jl",
+        name = "InitialValues",
+        url = "https://github.com/tkf/InitialValues.jl",
+        rev = "62b240fb85836b9731b0a893e446a9c90311afce",
     ); inline=false) isa Any
 end
